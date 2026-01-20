18b6985f10288bc67b1fdfe26d391a8199e9ae3d
Netprotec <libreprogramacion839@gmail.com>
2026-01-20 11:36:21 -0600

    Back 0.5

--- src/server.js
+++ src/server.js
@@ -31,3 +31,33 @@ app.listen(3000, '0.0.0.0', () => {
 });
 
 
+const express = require('express');
+const cors = require('cors');
+const router = require('./http/router');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowed = [
+      'http://154.38.163.221:4000'
+    ];
+
+    // Permite curl / postman (sin origin)
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS bloqueado'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(router);
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Backend remoto activo en http://154.38.163.221:3000');
+});
