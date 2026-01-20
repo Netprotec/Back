9ae7d72aba88fd6c48f073635b92d1c20eeda8fc
Netprotec <libreprogramacion839@gmail.com>
2026-01-20 11:53:08 -0600

    Version 7

--- src/server.js
+++ src/server.js
@@ -1,35 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./http/router');
-
-const app = express();
-
-app.use(express.json());
-
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowed = [
-      'http://localhost:4000',
-      'http://154.38.163.221:4000'
-    ];
-
-    // permite requests sin origin (curl, postman)
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS bloqueado'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.use(router);
-
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Servidor escuchando en http://0.0.0.0:3000');
-});
-
 
 const express = require('express');
 const cors = require('cors');
